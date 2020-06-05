fc6017555f6ab55913ddeb1ae20c3795fc365f0a
Rajneesh Shukla <RAJNEESH9651@GMAIL.COM>
2020-06-05 08:39:28 +0530

    Initial Project Setup

---
+++ eshopbackend/src/main/java/com/rajneesh/eshopbackend/App.java
@@ -0,0 +1,13 @@
+package com.rajneesh.eshopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
