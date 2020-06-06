79e161bd794557a52bf0092ef1ecb262c346777d
Rajneesh Shukla <RAJNEESH9651@GMAIL.COM>
2020-06-06 09:40:22 +0530

    - Add about, product list and contact page - Map about, product list and contact page url in controller

--- eshop/src/main/java/eshop/controller/PageController.java
+++ eshop/src/main/java/eshop/controller/PageController.java
@@ -10,7 +10,32 @@ public class PageController {
 	@RequestMapping(value = {"/", "/index", "/home"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Hello World!");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", "true");
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/about"})
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", "true");
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/listProducts"})
+	public ModelAndView listProducts() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Products List");
+		mv.addObject("userClickProductList", "true");
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", "true");
 		return mv;
 	}
 }
---
+++ eshop/src/main/webapp/WEB-INF/views/productsList.jsp
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="row">
+		<h1>Product List</h1>
+		<hr/>
+		<p>This is product list page</p>
+	</div>
+</div>
